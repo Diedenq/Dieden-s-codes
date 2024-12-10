caf19cd4a4621ab7976c1ad83e00d1b944e988e9
Diedenq <94123630+Diedenq@users.noreply.github.com>
2024-12-10 20:03:42 +0500

    BOT Заявок .py

--- BOT Заявок .py
+++ BOT Заявок .py
@@ -4,8 +4,8 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Используйте свои данные для токена бота и файла учетных данных Google
-TOKEN = '6515991106:AAHeYM0gNRcrMl2AMYW1-3qc_93bK8ejKEY'
-CREDENTIALS_FILE = 'D:\Рабочий стол/botproject2-5d542432eacc.json'
+TOKEN = ''
+CREDENTIALS_FILE = ''
 
 # Настройка Google Sheets
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -13,7 +13,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope
 client = gspread.authorize(creds)
 
 # Откройте таблицу по URL
-sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/15gA-0pAjHre8fHz_Dr0qjDAy6c2ouC3cIFNCRfPyQro/edit?hl=ru&pli=1#gid=0').sheet1
+sheet = client.open_by_url('').sheet1
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Привет! Пожалуйста, отправьте мне свое имя и номер в формате: Имя, Номер')
